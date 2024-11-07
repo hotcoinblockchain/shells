b702e46f6a6109c4e2750bdbeb8ab1dc56d931b8
walle <85925158+hotcoin-walle@users.noreply.github.com>
2024-11-07 13:55:00 +0800

    Create git.sh

---
+++ install/git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+### 
+# usage 
+# curl -sSL https://raw.githubusercontent.com/hotcoin-walle/shells/main/install/git.sh | bash -s 
+
+sudo apt update  
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git -y
