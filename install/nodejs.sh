3b4be4fe7dd709109c6c41a6bf067f8e351cc724
walle <85925158+hotcoin-walle@users.noreply.github.com>
2024-11-07 14:04:51 +0800

    Create nodejs.sh

---
+++ install/nodejs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install -y curl
+curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt update
+sudo apt-get install -y nodejs
