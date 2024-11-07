0e8e6550acd04c3634d1cda90034f9fd125272bd
walle <85925158+hotcoin-walle@users.noreply.github.com>
2024-11-07 14:07:28 +0800

    Update nodejs.sh

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# https://github.com/nodesource/distributions?tab=readme-ov-file#ubuntu-versions
+
 sudo apt-get install -y curl
 curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh
 sudo -E bash nodesource_setup.sh
