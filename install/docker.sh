b0037cdf2779e019a8ddba9ee345c81ca7e7d4a2
walle <85925158+hotcoin-walle@users.noreply.github.com>
2025-04-13 22:02:16 +0800

    Update docker.sh

--- install/docker.sh
+++ install/docker.sh
@@ -4,4 +4,6 @@
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
 curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
-curl -sSL https://get.daocloud.io/docker | sh
+# curl -sSL https://get.daocloud.io/docker | sh
+curl -L https://github.com/docker/compose/releases/download/v2.35.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x  /usr/local/bin/docker-compose
